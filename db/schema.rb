594a773517c69292b858762e4bee7a061050fa8e
ARM <alexander.michaud@aya.yale.edu>
2021-01-24 13:43:20 -0500

    Display products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_181302) do
+ActiveRecord::Schema.define(version: 2021_01_24_182031) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_181302) do
     t.integer "stock"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
 end
