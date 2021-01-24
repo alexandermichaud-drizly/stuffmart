a97d220088866642915359ac3840f0eaf42da80a
ARM <alexander.michaud@aya.yale.edu>
2021-01-24 14:30:29 -0500

    Product styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'products#index'
 
-  get 'products', to: 'products#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/products', to: 'products#index'
+  # resources :products 
 end
